be9e0ead9862b986d5efc743d48026f5b81d5d5f
Joshua Kennedy <josh@outthinkgroup.com>
2021-04-11 15:54:22 -0400

    clearing degug

--- winterland-presents-festival-pages.php
+++ winterland-presents-festival-pages.php
@@ -14,3 +14,9 @@ define('FESTIVAL_PAGES_CORE_URL', plugin_dir_url(__FILE__));
  add_action('init', function(){
    require FESTIVAL_PAGES_CORE_PATH . '/classes/class-festival-pages-core.php';
  });
+function add_acf_field_groups(){
+      include FESTIVAL_PAGES_CORE_PATH . 'acf/festival-options.php';
+      include FESTIVAL_PAGES_CORE_PATH . 'acf/festival.php';
+      
+}
+add_action('acf/init', 'add_acf_field_groups');
